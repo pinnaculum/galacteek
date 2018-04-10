a460a4f2b8d1a62f74503e8ef4a9630a991d9865
cipres <alkaline@gmx.co.uk>
2018-04-11 01:51:48 +0200

    cache module for ipfs entry caching. new functions in ipfsops (keys management and publish)

---
+++ galacteek/ipfs/cache.py
@@ -0,0 +1,19 @@
+import collections
+
+class IPFSEntryCache(object):
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self._cache = collections.OrderedDict()
+
+    def register(self, entry):
+        if entry['Hash'] not in self._cache:
+            self._cache[entry['Hash']] = entry
+
+    def __contains__(self, oHash):
+        return oHash in self._cache
+
+    def purge(self, oHash):
+        if oHash in self:
+            del self._cache[oHash]
