56a0c524f6ae48616e6c851baf73dee81599ca4e
cipres <BM-87dtCqLxqnpwzUyjzL8etxGK8MQQrhnxnt1@bitmessage>
2018-10-30 22:05:37 +0100

    revbump to 0.2.6

--- galacteek/__init__.py
+++ galacteek/__init__.py
@@ -18,7 +18,7 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 #
 
-__version__ = '0.2.5'
+__version__ = '0.2.6'
 
 from galacteek.core.asynclib import *
 from galacteek.core import glogger
