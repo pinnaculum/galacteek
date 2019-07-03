df6d7c508d866270a4e60ec2f81d356e31d97eaf
cipres <BM-87dtCqLxqnpwzUyjzL8etxGK8MQQrhnxnt1@bitmessage>
2019-07-03 19:59:20 +0200

    tox fixes + fetch go-ipfs before build. URL edit signal bugfix

--- travis/install.sh
+++ travis/install.sh
@@ -3,9 +3,23 @@
 PIP=pip3
 PYTHONEX=python3
 
+mkdir -p $HOME/bin
+
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
 	PYTHONEX=python3.6
+	wget https://dist.ipfs.io/go-ipfs/v0.4.21/go-ipfs_v0.4.21_linux-amd64.tar.gz
+	tar -C $HOME -xzvf go-ipfs_v0.4.21_linux-amd64.tar.gz
 fi
+
+if [ "$TRAVIS_OS_NAME" = "osx" ]; then
+	wget https://dist.ipfs.io/go-ipfs/v0.4.21/go-ipfs_v0.4.21_darwin-amd64.tar.gz
+	tar -C $HOME -xzvf go-ipfs_v0.4.21_darwin-amd64.tar.gz
+fi
+
+mv $HOME/go-ipfs/ipfs $HOME/bin
+
+export PYTHONPATH=$PYTHONPATH:/usr/local/lib/python3.6/dist-packages
+export PATH=$PATH:$HOME/bin
 
 $PYTHONEX -m venv venvg
 source venvg/bin/activate
@@ -13,15 +27,7 @@ source venvg/bin/activate
 $PIP install -r requirements.txt
 $PIP install -r requirements-dev.txt
 
-export PYTHONPATH=$PYTHONPATH:/usr/local/lib/python3.6/dist-packages
-
-if [ "$TRAVIS_OS_NAME" = "linux" ]; then
-	xfvb-run tox -v
-fi
-
-if [ "$TRAVIS_OS_NAME" = "osx" ]; then
-	tox -v
-fi
-
 $PYTHONEX setup.py build install
 $PYTHONEX setup.py sdist bdist_wheel
+
+tox -v
