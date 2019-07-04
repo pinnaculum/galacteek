eeb8a92ad7da5c612bab56c0f2463f39a4b00841
cipres <alkaline@gmx.co.uk>
2019-07-04 14:18:48 +0200

    travis: disable usage of UPLOADTOOL_BODY for now

--- travis/upload-images.sh
+++ travis/upload-images.sh
@@ -2,20 +2,6 @@
 
 set -x
 
-IFS='' read -r -d '' UPLOADTOOL_BODY <<"EOF"
-## galacteek release\n
-\n
-Download the AppImage (for Linux) or the DMG image (for macos)\n
-from below.\n
-\n
-After downloading the AppImage, make it executable and then run it:\n
-\n
-> chmod +x Galacteek-0.4.8-x86_64.AppImage\n
-./Galacteek-0.4.8-x86_64.AppImage\n
-EOF
-
-export UPLOADTOOL_BODY
-
 if [ "$TRAVIS_OS_NAME" = "linux" ]; then
 	cd AppImage
 	bash upload.sh Galacteek*.AppImage
