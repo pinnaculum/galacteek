a2805e0af98ac4a23ffbae975aa704a790583430
cipres <alkaline@gmx.co.uk>
2020-06-28 19:33:35 +0200

    AppImage: bundle libxcb from conda

--- travis/bootstrap-ubuntu.sh
+++ travis/bootstrap-ubuntu.sh
@@ -5,11 +5,10 @@ sudo add-apt-repository -y ppa:longsleep/golang-backports
 
 sudo apt-get update
 sudo apt-get install -y python3.7 python3.7-venv python3.7-dev python3-pip
-sudo apt-get install -y dzen2 libxkbcommon-x11-0 xvfb herbstluftwm libzbar0
+sudo apt-get install -y dzen2 libxcb-xkb1 libxkbcommon-x11-0 xvfb herbstluftwm libzbar0
 sudo apt-get install -y libpulse-mainloop-glib0 libpulse0
 
-sudo apt-get remove -y golang-1.11-go
-
-sudo apt install golang-1.14
+#sudo apt-get remove -y golang-1.11-go
+#sudo apt install golang-1.14
 
 python3.7 -m pip install pip
